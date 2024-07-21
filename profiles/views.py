a9e57fabe52fbd70862632a854cdd50adc7314cc
Morgan Asare <ohenemorgan@gmail.com>
2024-07-21 18:08:17 +0000

    added profiles detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response 
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Profile
+from django.http import Http404
 from .serializers import ProfileSerializer
 
 
@@ -9,4 +11,30 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+    def get_object(self, pk):
+        try: 
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404("This user ID does not exist")
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
+    
+    def put(self, request, pk):
+
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+        
 
